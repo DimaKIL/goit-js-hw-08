7a4859fbc56d9f88251a83dd5cf96f49b0bffb4d
Килимник Дмитро <dinakilimnik@gmail.com>
2022-10-04 11:03:19 +0300

    пп

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Add imports above this lin
+// Дополнительный импорт стилей
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+console.log(galleryItems);
+
+
+
+const globalDiv = document.querySelector('.gallery');
+for(let galleryItem of galleryItems){
+    globalDiv.insertAdjacentHTML('beforeend',`<a class="gallery__link" onclick="return false" href="${galleryItem.original}"> <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}"  alt="${galleryItem.description}" /></a>`);
+    console.log(globalDiv);
+}
+
+new SimpleLightbox('.gallery a');
 
 console.log(galleryItems);
