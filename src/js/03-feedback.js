c4a663783607f203a537dc063239aa6c8fe80c76
Килимник Дмитро <dinakilimnik@gmail.com>
2022-10-11 02:56:58 +0300

    good

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,51 @@
 import throttle from 'lodash.throttle';
 const inputRef = document.querySelector('.feedback-form');
 
-inputRef.addEventListener('input', throttle(onFormInput, 500));
-inputRef.addEventListener('submit', onSubmitForm);
+inputRef.addEventListener('input', throttle(onInput, 500));
+inputRef.addEventListener('submit', onSubmit);
 
 const tempKey = 'feedback-form-state';
 
-userSavedDataOutput();
+savedData();
 
-function onFormInput() {
+function onInput() {
   const userData = {
+
     email: inputRef.email.value,
+    
     message: inputRef.message.value,
+
   };
 
   const userDataStringify = JSON.stringify(userData);
   localStorage.setItem(tempKey, userDataStringify);
 }
 
-function userSavedDataOutput() {
+function savedData() {
+
   const savedUserData = JSON.parse(localStorage.getItem(tempKey));
+
   if (savedUserData) {
+
     inputRef.email.value = savedUserData.email;
     inputRef.message.value = savedUserData.message;
+
   }
+
 }
 
-function onSubmitForm(evt) {
+
+function onSubmit(evt) {
   if (!inputRef.email.value || !inputRef.message.value) {
     alert('Please, enter data in all fields');
     return;
   }
+
+
+
   evt.preventDefault();
+
+
 
   evt.currentTarget.reset();
   console.log(JSON.parse(localStorage.getItem(tempKey)));
