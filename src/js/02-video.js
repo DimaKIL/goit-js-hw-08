fad3ff086130301cd5e12573b8fe74bd85e5224c
Килимник Дмитро <dinakilimnik@gmail.com>
2022-10-11 00:23:48 +0300

    40%

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(iframeRef);
+
+player.on('timeupdate', throttle(timeToUpdate, 1000));
+
+const keyContainer = 'videoplayer - current - time';
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
 
